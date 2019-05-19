3d49dfe868aebecfb0df0bfff5c2d10c0f1f9c18
Adam <adam@dell-pc>
2019-05-19 12:49:31 +0200

    add filtering for noise

--- extractor.py
+++ extractor.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 
+from skimage.measure import ransac
+from skimage.transform import FundamentalMatrixTransform
+
 class Extractor(object):
     GX = 16//4
     GY = 12//4
@@ -24,7 +27,19 @@ class Extractor(object):
             matches = self.bf.knnMatch(des, self.last["des"], k=2)
             for m,n in matches:
                 if m.distance < 0.75*n.distance:
-                    ret.append((kps[m.queryIdx], self.last["kps"][m.trainIdx]))
-        
+                    kp1 = kps[m.queryIdx].pt
+                    kp2 = self.last["kps"][m.trainIdx].pt
+                    ret.append((kp1, kp2))
+
+        # filter
+        if len(ret) > 0:
+            ret = np.array(ret)
+            model, inliers = ransac((ret[:, 0], ret[:, 1]),
+                                    FundamentalMatrixTransform, 
+                                    min_samples=8,
+                                    residual_threshold=1, 
+                                    max_trials=100)
+            ret = ret[inliers]
+
         self.last = { "kps" : kps, "des" : des }
         return ret
